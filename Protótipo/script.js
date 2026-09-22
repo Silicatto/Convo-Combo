5608516d766abdc0f5128d309f6c942d85623c1a
kkaique <kaique.s.reis2009@gmail.com>
2026-09-22 16:23:40 -0300

    Add files via upload

---
+++ Protótipo/script.js
@@ -0,0 +1,22 @@
+let falas = {};
+falas = {
+    fala1: 'Cool! Mine is mathematics.',
+    fala2: 'Thank you very much!',
+    fala3: "It's this way.",
+    fala4: 'A crab!',
+    fala5: 'You might know!',
+    fala6: "I will check on that."
+};
+
+let respostaCorreta = 'fala1';
+
+function verify(fala){
+    if (fala == respostaCorreta){
+        $("#talk").text("Cool! Mine is mathematics.")
+        $("#talk2").text("Correct!");
+    }
+    else{
+        $("#talk").text("")
+        $("#talk2").text("Wrong!");
+    }
+}
